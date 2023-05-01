c0f6bf6425eda892f077ab7f738097275ba11084
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-05-02 01:38:35 +0300

    do

---
+++ sabak18_bmi_ulantuu2/lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:sabak18_bmi_ulantuu2/example.dart';
+import 'package:sabak18_bmi_ulantuu2/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: const MyHomePage(),
+    );
+  }
+}
