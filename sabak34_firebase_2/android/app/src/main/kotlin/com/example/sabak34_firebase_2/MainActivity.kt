75146082b4201f51f6512be37bd16372e1a8033c
adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2023-06-27 01:38:06 +0300

    done

---
+++ sabak34_firebase_2/android/app/src/main/kotlin/com/example/sabak34_firebase_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sabak34_firebase_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sabak34_firebase_2/lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:sabak34_firebase_2/views/home_view.dart';
+import 'package:sabak34_firebase_2/views/todo_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
